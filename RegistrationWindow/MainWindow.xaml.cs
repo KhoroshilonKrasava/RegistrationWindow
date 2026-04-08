026a659afc2d7b4476d62b7dc2c6cded64fd439c
Nikita <horoshilov19999@gmail.com>
2026-04-08 16:48:13 +0700

    Добавьте файлы проекта.

---
+++ RegistrationWindow/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+
+namespace RegistrationWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+      
+    }
+}
---
+++ RegistrationWindow/UserControls/PlaceHolderUserControl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace RegistrationWindow.UserControls
+{
+    public class PlaceHolderUserControl : TextBox
+    {
+        public string Placeholder { get; set; } = "Login";
+        public PlaceHolderUserControl()
+        {
+            GotFocus += OnGotFocus;
+            LostFocus += OnLostFocus;
+            Text += Placeholder;
+            Foreground = Brushes.Gray;
+        }
+        private void OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Text == Placeholder)
+            {
+                Text = "";
+                Foreground = Brushes.Black;
+            }
+        }
+
+        private void OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(Text))
+            {
+                Text = Placeholder;
+                Foreground = Brushes.Gray;
+            }
+        }
+    }
+}
