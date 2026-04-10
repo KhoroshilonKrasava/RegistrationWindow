c43227130a668c14258ae3b4cb7f6a124878e8fb
Nikita <horoshilov19999@gmail.com>
2026-04-10 15:26:58 +0700

    БД

---
+++ RegistrationWindow/DataBase/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using RegistrationWindow.DataBase.Models;
+
+namespace RegistrationWindow.DataBase
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<UserModel> Users => Set<UserModel>();
+        public ApplicationContext() => Database.EnsureCreated();
+
+        static readonly string connectionString = "Server=localhost; User ID=root; Password=123456; Database=MonitorDataBase";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+    }
+}
---
+++ RegistrationWindow/DataBase/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegistrationWindow.DataBase.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
+
+    }
+}
